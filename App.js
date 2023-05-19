dac6d9dc82c8e88b7a3693c015f4baa95db24813
KutluhanOzbakan <kutluhanozbakan21@gmail.com>
2023-05-19 12:19:45 +0300

    Adding routers

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    <StatusBar style='light'></StatusBar>
-    <CategoriesScreen/>
+      <StatusBar style="light"></StatusBar>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={CategoriesScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
