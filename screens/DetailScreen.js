be83beabc5d09f9386bb93b75be7600b68e53154
KutluhanOzbakan <kutluhanozbakan21@gmail.com>
2023-05-21 14:36:37 +0300

    entegrate redux

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -13,16 +13,29 @@ import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import IconButton from "../components/IconButton";
 import { FavoritesContext } from "../store/context/favorites-context";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavorite, removeFavorite } from "../store/redux/favorites";
 function DetailScreen({ route, navigation }) {
-  const favoriteMealsCtx = useContext(FavoritesContext);
 
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+  const dispatch = useDispatch();
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
-  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+
+  // const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
+
 
   function changeFavoriteStatusHandler() {
-    if (mealIsFavorite) favoriteMealsCtx.removeFavorite(mealId);
-    else favoriteMealsCtx.addFavorite(mealId);
+    if(mealIsFavorite) {
+      // favoriteMealsCtx.removeFavorite(mealId);
+      dispatch(removeFavorite({id: mealId}));
+    } 
+    else {
+      // favoriteMealsCtx.addFavorite(mealId);
+      dispatch(addFavorite({id: mealId}));
+    }
   }
 
   useLayoutEffect(() => {
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -3,27 +3,34 @@ import MealsList from "../components/MealList/MealList";
 import { useContext, useState } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
 function FavoritesScreen(params) {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  //   const favoriteMealsCtx = useContext(FavoritesContext);
+  //   const favoriteMeals = MEALS.filter((meal) =>
+  //     favoriteMealsCtx.ids.includes(meal.id)
+  //   );
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
-  if(favoriteMeals.length === 0)
-    return <View style={styles.rootContainer}>
+  if (favoriteMeals.length === 0)
+    return (
+      <View style={styles.rootContainer}>
         <Text style={styles.text}>No favorite meals yet.</Text>
-    </View>
+      </View>
+    );
   return <MealsList items={favoriteMeals} />;
 }
 export default FavoritesScreen;
 const styles = StyleSheet.create({
-    rootContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    text:{
-        fontSize: 18,
-        fontWeight: "bold",
-        color: 'white'
-    }
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
 });
